6d05fabaf3523f3ca471477b6e036a19dc10af21
Sanjay Purswani <sanjsanj@hotmail.com>
2017-04-02 11:08:47 +0100

    Can take screenshots for multiple tests with error handling

--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -7,43 +7,72 @@ describe("Credit cards", function() {
     var pegCreditCardsUrl = "http://peg.money.comparethemarket.com/credit-cards/";
     var expectedTitle = "Compare Our Best Credit Card Deals | CompareTheMarket";
     var filterList = ".basic-filter-list";
+    var testDate = Date();
 
-    // it("should have the right title ", function () {
-    //     browser.url(pegCreditCardsUrl);
-    //     var actualTitle = browser.getTitle();
-    //     assert.equal(actualTitle, expectedTitle);
-    // });
+    it("should load ok and with the right title ", function () {
+        browser.url(pegCreditCardsUrl);
+
+        var browserState = browser.status().state;
+        expect(browserState).to.eql("success");
+
+        var actualTitle = browser.getTitle();
+        expect(actualTitle).to.eql(expectedTitle);
+    });
 
     describe("From the overview page", function() {
-        // it("can click on a 0% balance transfer card and be taken to its page", function () {
-        //     browser.url(pegCreditCardsUrl);
-        //     browser.click("#balance-transfer-credit-card-id-87");
-        //     var card = $("input[id=CreditCard_GoTo_Provider_87]");
-        //     expect(card).to.exist;
-        // });
+        it("can click on a 0% balance transfer card and be taken to its page", function () {
+            browser.url(pegCreditCardsUrl);
+            browser.click("#balance-transfer-credit-card-id-87");
+            var card = $("input[id=CreditCard_GoTo_Provider_87]");
+            expect(card).to.exist;
+        });
 
-        it("can click on a 0% balance transfer card and go through to the third party", function () {
+        it("can click on a 0% balance transfers card and go through to the third party", function () {
             browser.url(pegCreditCardsUrl);
             browser.click('#balance-transfer-credit-card-id-87');
             browser.click("input#CreditCard_GoTo_Provider_87");
-            browser.waitForVisible(".apply-with-no-rewards-action", 10000);
-            browser.click(".apply-with-no-rewards-action");
-            browser.switchTab(browser.getTabIds()[1]);
+
+            try {
+                browser.waitForVisible(".apply-with-no-rewards-action", 10000);
+                browser.click(".apply-with-no-rewards-action");
+            } catch (event) {
+                console.log(event);
+            }
+
+            browser.switchTab(browser.getTabIds().slice(-1)[0]);
 
             var browserState = browser.status().state;
             expect(browserState).to.eql("success");
 
-            var testDate = Date();
-            fs.mkdir(`./screenshots/${testDate}`);
-            browser.saveScreenshot(`./screenshots/${testDate}/${Date()}.png`);
+            if (!fs.existsSync(`./screenshots/${testDate}`)) {
+                fs.mkdir(`./screenshots/${testDate}`);
+            }
+
+            browser.saveScreenshot(`./screenshots/${testDate}/${Date()}_balance.png`);
         });
 
-        // it("can click on a ... card and be taken to its page", function () {
-        //     browser.url(pegCreditCardsUrl);
-        //     browser.click(filterList);
-        //     browser.click("#balance-transfer-credit-card-id-93");
-        //     var card = browser.getHTML("#CreditCard_GoTo_Provider_93");
-        //     assert.notEqual(card, "undefined");
-        // });
+        it("can click on a 0% balance transfers and purchases card and go through to the third party", function () {
+            browser.url(pegCreditCardsUrl);
+            browser.click('#balance-transfer-and-new-purchase-credit-card-id-93');
+            browser.click("input#CreditCard_GoTo_Provider_93");
+
+            try {
+                browser.waitForVisible(".apply-with-no-rewards-action", 10000);
+                browser.click(".apply-with-no-rewards-action");
+            } catch (event) {
+                console.log(event);
+            }
+
+            browser.switchTab(browser.getTabIds().slice(-1)[0]);
+
+            var browserState = browser.status().state;
+            expect(browserState).to.eql("success");
+
+            if (!fs.existsSync(`./screenshots/${testDate}`)) {
+                fs.mkdir(`./screenshots/${testDate}`);
+            }
+
+            browser.saveScreenshot(`./screenshots/${testDate}/${Date()}_balance_and_purchase.png`);
+        });
     });
 });
